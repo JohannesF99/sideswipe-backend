e496240c99e7cfa4e51608a84c7250f0cc44016d
Johannes Franke <jofra@hotmail.de>
2022-04-13 20:56:46 +0200

    added exception handling for unauthorized request

--- src/main/kotlin/de/johannes/sideswipe/controller/UserDataController.kt
+++ src/main/kotlin/de/johannes/sideswipe/controller/UserDataController.kt
@@ -4,7 +4,9 @@ import de.johannes.sideswipe.enums.Gender
 import de.johannes.sideswipe.model.UserData
 import de.johannes.sideswipe.repositories.UserDataRepository
 import org.apache.logging.log4j.LogManager
+import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.*
+import org.springframework.web.server.ResponseStatusException
 
 @RestController
 @RequestMapping("api/v1/user")
@@ -13,8 +15,15 @@ class UserDataController(private val userDataRepository: UserDataRepository) {
     private val logger = LogManager.getLogger()
 
     @GetMapping("/{username}")
-    fun getUserData(@PathVariable username: String): UserData?{
-        return userDataRepository.findByUsername(username)
+    fun getUserData(@PathVariable username: String): UserData{
+        try {
+            val user = userDataRepository.findByUsername(username)
+            logger.info("User-Data requested for \"$username\"")
+            return user
+        } catch (e: Exception){
+            logger.warn("User-Data requested for unknown Username \"$username\"")
+            throw ResponseStatusException(HttpStatus.BAD_REQUEST, "User-Data requested for unknown Username \"$username\"", e)
+        }
     }
 
     @GetMapping("/all")
@@ -24,33 +33,53 @@ class UserDataController(private val userDataRepository: UserDataRepository) {
 
     @PutMapping("/{username}/name")
     fun updateUserDataName(@RequestBody name: String, @PathVariable username: String): UserData{
-        val user = userDataRepository.findByUsername(username)
-        logger.info("User \"$username\" changed his last name from \"${user.name}\" to \"$name\"!")
-        user.name = name
-        return userDataRepository.save(user)
+        try {
+            val user = userDataRepository.findByUsername(username)
+            logger.info("User \"$username\" changed his last name from \"${user.name}\" to \"$name\"!")
+            user.name = name
+            return userDataRepository.save(user)
+        } catch (e: Exception){
+            logger.warn("Last Name change for User-Data requested for unknown Username \"$username\"")
+            throw ResponseStatusException(HttpStatus.BAD_REQUEST, "Last Name change for User-Data requested for unknown Username \"$username\"", e)
+        }
     }
 
     @PutMapping("/{username}/vorname")
     fun updateUserDataVorname(@RequestBody vorname: String, @PathVariable username: String): UserData{
-        val user = userDataRepository.findByUsername(username)
-        logger.info("User \"$username\" changed his first name from \"${user.vorname}\" to \"$vorname\"!")
-        user.vorname = vorname
-        return userDataRepository.save(user)
+        try {
+            val user = userDataRepository.findByUsername(username)
+            logger.info("User \"$username\" changed his first name from \"${user.vorname}\" to \"$vorname\"!")
+            user.vorname = vorname
+            return userDataRepository.save(user)
+        } catch (e: Exception){
+            logger.warn("First Name change for User-Data requested for unknown Username \"$username\"")
+            throw ResponseStatusException(HttpStatus.BAD_REQUEST, "First Name change for User-Data requested for unknown Username \"$username\"", e)
+        }
     }
 
     @PutMapping("/{username}/gender")
     fun updateUserDataGender(@RequestBody gender: Gender, @PathVariable username: String): UserData{
-        val user = userDataRepository.findByUsername(username)
-        logger.info("User \"$username\" changed his gender from \"${user.gender}\" to \"$gender\"!")
-        user.gender = gender
-        return userDataRepository.save(user)
+        try {
+            val user = userDataRepository.findByUsername(username)
+            logger.info("User \"$username\" changed his gender from \"${user.gender}\" to \"$gender\"!")
+            user.gender = gender
+            return userDataRepository.save(user)
+        } catch (e: Exception){
+            logger.warn("Gender change for User-Data requested for unknown Username \"$username\"")
+            throw ResponseStatusException(HttpStatus.BAD_REQUEST, "Gender change for User-Data requested for unknown Username \"$username\"", e)
+        }
     }
 
     @PutMapping("/{username}/birthday")
     fun updateUserDataBirthday(@RequestBody birthday: String, @PathVariable username: String): UserData{
-        val user = userDataRepository.findByUsername(username)
-        logger.info("User \"$username\" changed his date of birth from \"${user.birthday}\" to \"$birthday\"!")
-        user.birthday = birthday
-        return userDataRepository.save(user)
+        try {
+            val user = userDataRepository.findByUsername(username)
+            logger.info("User \"$username\" changed his date of birth from \"${user.birthday}\" to \"$birthday\"!")
+            user.birthday = birthday
+            return userDataRepository.save(user)
+        } catch (e: Exception){
+            logger.warn("Birthday change for User-Data requested for unknown Username \"$username\"")
+            throw ResponseStatusException(HttpStatus.BAD_REQUEST, "Birthday change for User-Data requested for unknown Username \"$username\"", e)
+        }
     }
 }
