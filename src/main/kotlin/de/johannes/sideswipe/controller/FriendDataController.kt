6d0c323345336c8f0ea1cff8bf661e1e56bc0a9f
Johannes Franke <jofra@hotmail.de>
2022-04-13 21:32:52 +0200

    added exception handling for unauthorized request

--- src/main/kotlin/de/johannes/sideswipe/controller/FriendDataController.kt
+++ src/main/kotlin/de/johannes/sideswipe/controller/FriendDataController.kt
@@ -4,7 +4,9 @@ import de.johannes.sideswipe.model.FriendData
 import de.johannes.sideswipe.repositories.FriendDataRepository
 import de.johannes.sideswipe.repositories.UserDataRepository
 import org.apache.logging.log4j.LogManager
+import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.*
+import org.springframework.web.server.ResponseStatusException
 
 @RestController
 @RequestMapping("api/v1/user/{username}/friend")
@@ -16,25 +18,41 @@ class FriendDataController(
 
     @PostMapping
     fun changeFriend(@PathVariable username: String, @RequestBody friendName: String) {
-        val user = userDataRepository.findByUsername(username)
-        val friend = userDataRepository.findByUsername(friendName)
-        if (!friendDataRepository.existsByUserIdAndFriendData(user.userId, friend)) {
-            friendDataRepository.save(FriendData(user, friend))
-            logger.info("User \"$username\" added \"$friendName\" to his friends!")
+        try {
+            val user = userDataRepository.findByUsername(username)
+            val friend = userDataRepository.findByUsername(friendName)
+            if (!friendDataRepository.existsByUserIdAndFriendData(user.userId, friend)) {
+                friendDataRepository.save(FriendData(user, friend))
+                logger.info("User \"$username\" added \"$friendName\" to his friends!")
+            }
+        } catch (e: Exception){
+            logger.warn("Friend-Request unsuccessful, because User-ID \"$username\" or Friend \"$friendName\" could not be found!")
+            throw ResponseStatusException(HttpStatus.BAD_REQUEST, "Friend-Request unsuccessful, because User-ID \"$username\" or Friend \"$friendName\" could not be found!", e)
         }
     }
 
     @DeleteMapping
     fun deleteFriend(@PathVariable username: String, @RequestBody friendName: String){
-        val user = userDataRepository.findByUsername(username)
-        val friend = userDataRepository.findByUsername(friendName)
-        logger.info("User \"$username\" removed \"$friendName\" from his friends!")
-        return friendDataRepository.deleteByUserIdAndFriendData(user.userId, friend)
+        try {
+            val user = userDataRepository.findByUsername(username)
+            val friend = userDataRepository.findByUsername(friendName)
+            logger.info("User \"$username\" removed \"$friendName\" from his friends!")
+            return friendDataRepository.deleteByUserIdAndFriendData(user.userId, friend)
+        } catch (e: Exception){
+            logger.warn("Removal of Friend-Request unsuccessful, because User-ID \"$username\" or Friend \"$friendName\" could not be found!")
+            throw ResponseStatusException(HttpStatus.BAD_REQUEST, "Removal of Friend-Request unsuccessful, because User-ID \"$username\" or Friend \"$friendName\" could not be found!", e)
+        }
     }
 
     @GetMapping("/all")
     fun getFriendsForUserData(@PathVariable username: String): Set<FriendData> {
-        val user = userDataRepository.findByUsername(username)
-        return friendDataRepository.findAllByUserId(user.userId)
+        try {
+            val user = userDataRepository.findByUsername(username)
+            logger.info("Fetched Friends-List for Username \"$username\"!")
+            return friendDataRepository.findAllByUserId(user.userId)
+        } catch (e: Exception){
+            logger.warn("Could not fetch Friends-List for unknown Username \"$username\"!")
+            throw  ResponseStatusException(HttpStatus.BAD_REQUEST, "Could not fetch Friends-List for unknown Username \"$username\"!", e)
+        }
     }
 }
