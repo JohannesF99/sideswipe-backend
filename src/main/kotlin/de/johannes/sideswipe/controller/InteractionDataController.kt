1525f41ce50fba86df4368e861a9b2eeae56722d
Johannes Franke <jofra@hotmail.de>
2022-04-13 21:22:08 +0200

    added exception handling for unauthorized request

--- src/main/kotlin/de/johannes/sideswipe/controller/InteractionDataController.kt
+++ src/main/kotlin/de/johannes/sideswipe/controller/InteractionDataController.kt
@@ -5,7 +5,9 @@ import de.johannes.sideswipe.repositories.ContentDataRepository
 import de.johannes.sideswipe.repositories.InteractionDataRepository
 import de.johannes.sideswipe.repositories.UserDataRepository
 import org.apache.logging.log4j.LogManager
+import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.*
+import org.springframework.web.server.ResponseStatusException
 
 @RestController
 @RequestMapping("api/v1/user/{username}/interaction")
@@ -16,12 +18,18 @@ class InteractionDataController(
 ) {
     private val logger = LogManager.getLogger()
 
-    @GetMapping("/{contendId}")
-    fun getAllInteractionsForContent(@PathVariable contendId: Int, @PathVariable username: String): List<Map<String, Boolean>> {
-        val content = contentDataRepository.findByContentId(contendId.toLong())
-        return interactionDataRepository.findAllByContentData(content)
-            .sortedBy { it.isLike }
-            .map { mapOf(Pair(it.userData!!.username, it.isLike)) }
+    @GetMapping("/{contentId}")
+    fun getAllInteractionsForContent(@PathVariable contentId: Int, @PathVariable username: String): List<Map<String, Boolean>> {
+        try {
+            val content = contentDataRepository.findByContentId(contentId.toLong())
+            logger.info("Interaction-Data requested for Content-ID \"$contentId\"")
+            return interactionDataRepository.findAllByContentData(content)
+                .sortedBy { it.isLike }
+                .map { mapOf(Pair(it.userData!!.username, it.isLike)) }
+        } catch (e: Exception){
+            logger.warn("Interaction-Data requested for unknown Content-ID \"$contentId\"")
+            throw ResponseStatusException(HttpStatus.BAD_REQUEST,"Interaction-Data requested for unknown Content-ID \"$contentId\"", e)
+        }
     }
 
     @GetMapping("/all")
@@ -34,29 +42,44 @@ class InteractionDataController(
 
     @PostMapping("/{contentId}/like")
     fun addLikeInteraction(@PathVariable contentId: Int, @PathVariable username: String): InteractionData {
-        val content = contentDataRepository.findByContentId(contentId.toLong())
-        val user = userDataRepository.findByUsername(username)
-        val interactionData = interactionDataRepository.findByUserDataAndContentData(user, content) ?: InteractionData(true, user, content)
-        interactionData.isLike = true
-        logger.info("User \"$username\" liked the Post with Content ID $contentId!")
-        return interactionDataRepository.save(interactionData)
+        try {
+            val content = contentDataRepository.findByContentId(contentId.toLong())
+            val user = userDataRepository.findByUsername(username)
+            val interactionData = interactionDataRepository.findByUserDataAndContentData(user, content) ?: InteractionData(true, user, content)
+            interactionData.isLike = true
+            logger.info("User \"$username\" liked the Post with Content ID $contentId!")
+            return interactionDataRepository.save(interactionData)
+        } catch (e: Exception){
+            logger.warn("Like-Interaction unavailable, because Content-ID \"$contentId\" or Username \"$username\" could not be found!")
+            throw ResponseStatusException(HttpStatus.BAD_REQUEST,"Like-Interaction unavailable, because Content-ID \"$contentId\" or Username \"$username\" could not be found!", e)
+        }
     }
 
     @PostMapping("/{contentId}/dislike")
     fun addDislikeInteraction(@PathVariable contentId: Int, @PathVariable username: String): InteractionData {
-        val content = contentDataRepository.findByContentId(contentId.toLong())
-        val user = userDataRepository.findByUsername(username)
-        val interactionData = interactionDataRepository.findByUserDataAndContentData(user, content) ?: InteractionData(false, user, content)
-        interactionData.isLike = false
-        logger.info("User \"$username\" disliked the Post with Content ID $contentId!")
-        return interactionDataRepository.save(interactionData)
+        try {
+            val content = contentDataRepository.findByContentId(contentId.toLong())
+            val user = userDataRepository.findByUsername(username)
+            val interactionData = interactionDataRepository.findByUserDataAndContentData(user, content) ?: InteractionData(false, user, content)
+            interactionData.isLike = false
+            logger.info("User \"$username\" disliked the Post with Content ID $contentId!")
+            return interactionDataRepository.save(interactionData)
+        } catch (e: Exception){
+            logger.warn("Dislike-Interaction unavailable, because Content-ID \"$contentId\" or Username \"$username\" could not be found!")
+            throw ResponseStatusException(HttpStatus.BAD_REQUEST,"Dislike-Interaction unavailable, because Content-ID \"$contentId\" or Username \"$username\" could not be found!", e)
+        }
     }
 
     @DeleteMapping("/{contentId}/remove")
     fun removeInteraction(@PathVariable contentId: Int, @PathVariable username: String) {
-        val content = contentDataRepository.findByContentId(contentId.toLong())
-        val user = userDataRepository.findByUsername(username)
-        logger.info("User \"$username\" removed his interaction of Post with Content ID $contentId!")
-        return interactionDataRepository.deleteByUserDataAndContentData(user, content)
+        try {
+            val content = contentDataRepository.findByContentId(contentId.toLong())
+            val user = userDataRepository.findByUsername(username)
+            logger.info("User \"$username\" removed his interaction of Post with Content ID $contentId!")
+            return interactionDataRepository.deleteByUserDataAndContentData(user, content)
+        } catch (e: Exception){
+            logger.warn("Interaction could not be removed, because Content-ID \"$contentId\" or Username \"$username\" could not be found!")
+            throw ResponseStatusException(HttpStatus.BAD_REQUEST,"Interaction could not be removed, because Content-ID \"$contentId\" or Username \"$username\" could not be found!", e)
+        }
     }
 }
