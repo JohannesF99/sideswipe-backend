32f00b34ce1622c67d7e7facd05715ca9d1dc001
Johannes Franke <jofra@hotmail.de>
2022-04-13 21:58:18 +0200

    added exception handling for unauthorized request

--- src/main/kotlin/de/johannes/sideswipe/controller/ContentDataController.kt
+++ src/main/kotlin/de/johannes/sideswipe/controller/ContentDataController.kt
@@ -5,8 +5,9 @@ import de.johannes.sideswipe.repositories.ContentDataRepository
 import de.johannes.sideswipe.repositories.FriendDataRepository
 import de.johannes.sideswipe.repositories.UserDataRepository
 import org.apache.logging.log4j.LogManager
-import org.springframework.data.repository.findByIdOrNull
+import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.*
+import org.springframework.web.server.ResponseStatusException
 
 @RestController
 @RequestMapping("api/v1/user/{username}/content")
@@ -19,46 +20,80 @@ class ContentDataController(
 
     @PostMapping
     fun saveContent(@RequestBody contentData: ContentData, @PathVariable username: String): ContentData {
-        val user = userDataRepository.findByUsername(username)
-        val content = contentDataRepository.save(ContentData(contentData, user))
-        logger.info("User \"$username\" created new Content with ID ${content.contentId}")
-        return content
+        try {
+            val user = userDataRepository.findByUsername(username)
+            val content = contentDataRepository.save(ContentData(contentData, user))
+            logger.info("User \"$username\" created new Content with ID ${content.contentId}")
+            return content
+        } catch (e: Exception) {
+            logger.warn("Could not create new Content, because of unknown Username \"$username\"!")
+            throw ResponseStatusException(HttpStatus.BAD_REQUEST, "Could not create new Content, because of unknown Username \"$username\"!", e)
+        }
     }
 
     @GetMapping("/all")
     fun getAllContent(@PathVariable username: String): Set<ContentData> {
-        val user = userDataRepository.findByUsername(username)
-        return contentDataRepository.findAllByUserData(user)
+        try {
+            val user = userDataRepository.findByUsername(username)
+            logger.info("Fetched Content-List for Username \"$username\"!")
+            return contentDataRepository.findAllByUserData(user)
+        } catch (e : Exception) {
+            logger.warn("Could not fetch Content-List, because of unknown Username \"$username\"!")
+            throw ResponseStatusException(HttpStatus.BAD_REQUEST, "Could not fetch Content-List, because of unknown Username \"$username\"!", e)
+        }
     }
 
     @GetMapping("/{contentId}")
-    fun getContent(@PathVariable username: String, @PathVariable contentId: Int): ContentData? {
-        return contentDataRepository.findByIdOrNull(contentId.toLong())
+    fun getContent(@PathVariable username: String, @PathVariable contentId: Int): ContentData {
+        try {
+            val content = contentDataRepository.findByContentId(contentId.toLong())
+            logger.info("Fetched Content for Content-ID \"$contentId\"!")
+            return content
+        } catch (e: Exception) {
+            logger.warn("Could not fetch Content for unknown Content-ID \"$contentId\"!")
+            throw ResponseStatusException(HttpStatus.BAD_REQUEST, "Could not fetch Content for unknown Content-ID \"$contentId\"!", e)
+        }
     }
 
     @PutMapping("/{contentId}")
     fun changeContent(@PathVariable username: String, @PathVariable contentId: Int, @RequestBody newCaption: String): ContentData {
-        val content = contentDataRepository.findByContentId(contentId.toLong())
-        content.caption = newCaption
-        logger.info("Caption of Content ID ${content.contentId} got changed!")
-        return contentDataRepository.save(content)
+        try {
+            val content = contentDataRepository.findByContentId(contentId.toLong())
+            content.caption = newCaption
+            logger.info("Caption of Content-ID ${content.contentId} got changed!")
+            return contentDataRepository.save(content)
+        } catch (e: Exception) {
+            logger.warn("Could not change Content-Caption for unknown Content-ID \"$contentId\"!")
+            throw ResponseStatusException(HttpStatus.BAD_REQUEST, "Could not change Content-Caption for unknown Content-ID \"$contentId\"!", e)
+        }
     }
 
     @DeleteMapping("/{contentId}")
     fun deleteContent(@PathVariable username: String, @PathVariable contentId: Int){
-        logger.info("Post with Content ID $contentId got deleted!")
-        contentDataRepository.deleteById(contentId.toLong())
+        try {
+            logger.info("Post with Content ID $contentId got deleted!")
+            contentDataRepository.deleteById(contentId.toLong())
+        } catch (e: Exception) {
+            logger.warn("Could not delete Content for unknown Content-ID \"$contentId\"!")
+            throw ResponseStatusException(HttpStatus.BAD_REQUEST, "Could not delete Content for unknown Content-ID \"$contentId\"!", e)
+        }
     }
 
     @GetMapping("/friends")
     fun getAllFriendsContent(@PathVariable username: String): List<ContentData> {
-        val user = userDataRepository.findByUsername(username)
-        return friendDataRepository.findAllByUserId(user.userId)
-            .asSequence()
-            .map { contentDataRepository.findAllByUserData(it.friendData!!) }
-            .map { it.take(10) }
-            .flatten()
-            .sortedBy { it.lastModified }
-            .toList()
+        try {
+            val user = userDataRepository.findByUsername(username)
+            logger.warn("Fetched Content of Friends for Username \"$username\"!")
+            return friendDataRepository.findAllByUserId(user.userId)
+                .asSequence()
+                .map { contentDataRepository.findAllByUserData(it.friendData!!) }
+                .map { it.take(10) }
+                .flatten()
+                .sortedBy { it.lastModified }
+                .toList()
+        } catch (e: Exception) {
+            logger.warn("Could not fetch Content of Friends for unknown Username \"$username\"!")
+            throw ResponseStatusException(HttpStatus.BAD_REQUEST, "Could not fetch Content of Friends for unknown Username \"$username\"!", e)
+        }
     }
 }
