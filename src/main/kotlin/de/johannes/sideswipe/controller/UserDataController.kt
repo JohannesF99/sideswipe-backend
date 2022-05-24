01a5cabf26b2e946c85f8bc48eac5c8b5dd61fcd
Johannes Franke <jofra@hotmail.de>
2022-05-24 21:20:16 +0200

    -let user retrieve infos about other users

--- src/main/kotlin/de/johannes/sideswipe/controller/UserDataController.kt
+++ src/main/kotlin/de/johannes/sideswipe/controller/UserDataController.kt
@@ -1,6 +1,5 @@
 package de.johannes.sideswipe.controller
 
-import de.johannes.sideswipe.enums.Gender
 import de.johannes.sideswipe.model.UserData
 import de.johannes.sideswipe.repositories.UserDataRepository
 import org.apache.logging.log4j.LogManager
@@ -19,7 +18,8 @@ class UserDataController(private val userDataRepository: UserDataRepository) {
     fun getUserData(@PathVariable username: String): UserData{
         try {
             val user = userDataRepository.findByUsername(username)
-            user.doesTokenMatch()
+            //Enable if User should only fetch their own UserData
+            //user.doesTokenMatch()
             logger.info("User-Data requested for Username '$username'!")
             return user
         } catch (e: EmptyResultDataAccessException){
